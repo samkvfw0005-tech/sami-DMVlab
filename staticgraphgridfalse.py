f1473c3e78cf69a8f174559ca0b9b4911b17059f
samkvfw0005-tech <samkvfw0005@gmail.com>
2026-03-13 10:29:08 +0530

    Add files via upload

---
+++ staticgraphgridfalse.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 15, 25, 30]
+
+plt.plot(x, y, marker='o')
+
+plt.xlabel("X values (Independent Variable)")
+plt.ylabel("Y values (Dependent Variable)")
+plt.title("Static Graph - Grid False")
+
+plt.grid(False)
+
+plt.show()
